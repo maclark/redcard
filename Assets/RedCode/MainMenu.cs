ef5cdcfd71a5e72484638b1a3772ff296900c528
maxalanclark <maxaclark@gmail.com>
2025-11-16 17:22:16 -0800

    Got main menu referee whistle dangling well

--- Assets/RedCode/MainMenu.cs
+++ Assets/RedCode/MainMenu.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MainMenu : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+namespace RedCard {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public class MainMenu : MonoBehaviour {
+
+        public Rigidbody rbWhistle;
+        public float bumpPower = 0.25f;
+
+
+        private void Update() {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+                Vector3 randomDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                randomDir.Normalize();
+                if (rbWhistle) rbWhistle.AddForce(bumpPower * randomDir, ForceMode.Impulse);
+            }
+        }
     }
 }
