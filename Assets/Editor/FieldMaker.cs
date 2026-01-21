65df1eb19f5ddc289ac47c7454d7f4e02ad34048
MACLARK-ASUS\maxac <maxaclark@gmail.com>
2026-01-21 14:54:10 -0800

    Renamed mats folder Created medium and dark grasses with noise and started laying out field

---
+++ Assets/Editor/FieldMaker.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+
+
+namespace RedCard {
+
+    public class FieldMaker : MonoBehaviour {
+        public float xPatch = 6f;
+        public float yPatch = 6f;
+
+    }
+
+}
